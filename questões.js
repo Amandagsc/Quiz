11ddfd0528e1cffb76b84d0c52aab66329963643
Amandagsc <70444717+Amandagsc@users.noreply.github.com>
2020-12-06 22:52:44 -0300

    mudei o README

---
+++ questões.js
@@ -0,0 +1,169 @@
+//criando um array e passando o número de perguntas e opções de respostas
+let questions = [
+    {
+        numb: 1,
+        question: "Uma calcinha preta e de renda foi parar em um quadro no andar de cirurgia. De quem era a calcinha?",
+        answer: "Meredith Grey",
+        options: [
+            "Meredith Grey",
+            "Arizona Robbins",
+            "Miranda Bailey",
+            "Callie torres"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Qual cirurgião/cirurgiã operou o marido da Dra. Bailey no dia em que ela estava parindo o seu filho? E quem a ajudou no parto?",
+        answer: "Dr. Shepherd e Dr. O'Malley",
+        options: [
+            "Dr. Shepherd e Dr. O'Malley",
+            "Dr. Torres e Dr. Karev",
+            "Dr. Weber e Dra. Yang",
+            "Dr. Burker e Dr. O'Malley"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Em qual temporada o George O'Malley morre?",
+        answer: "Temporada 5",
+        options: [
+            "Temporada 11",
+            "Temporada 5",
+            "Temporada 3",
+            "Temporada 7"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Qual era o nome do cachorro da Merdith e do Derek?",
+        answer: "Doc",
+        options: [
+            "Tobi",
+            "Scooby",
+            "spike",
+            "Doc"
+        ]
+    },
+    {
+    numb: 5,
+        question: "Qual desses personagens ESTAVAM no acidente do avião??",
+        answer: "Mark Sloan e Lexie Grey",
+        options: [
+            "Ted Altman e April Kepner",
+            "Mark Sloan e Lexie Grey",
+            "Richard Webber e Jackson Every",
+            "Alex Karev e Addison Shepherd"
+        ]
+    },
+    {
+        numb: 6,
+            question: "Na 1 temporada, uma paciente chamada Katie Bryce tinha um problema que ninguém conseguiu descobrir, exceto a Meredith. Ela teve ajuda. Quem a ajudou?",
+            answer: "Dra. Yang",
+            options: [
+                "Dr. O'Malley",
+                "Dra. Bailey",
+                "Dr. Karev",
+                "Dra. Yang"
+            ]
+    },
+    {
+        numb: 7,
+            question: "Quando Meredith foi espancada por um paciente com problemas neurológicos, quem a achou caída no chão ao lado do paciente que a agrediu?",
+            answer: "Penny",
+            options: [
+                "Teddy",
+                "Owen",
+                "Amélia",
+                "Penny"
+        ]
+    },
+    {
+        numb: 8,
+            question: "Quem amputou a perna da Arizona?",
+            answer: "Alex Karev",
+            options: [
+                "Callie torres",
+                "Miranda Bailey",
+                "Alex Karev",
+                "Richard Webber"
+        ]
+    },
+    {
+        numb: 9,
+            question: "Qual das alternativas a seguir não foi nome do hospital?",
+            answer: "Hospital Memorial Shepherd",
+            options: [
+                "Seattle grace Hospital",
+                "Hospital de Seattle grace Mercy west",
+                "Hospital Memorial Grey Sloan",
+                "Hospital Memorial Shepherd"
+        ]
+    },
+    {
+        numb: 10,
+            question: "Quem NÃO morou na casa da mãe da Meredith?",
+            answer: "Teddy Altman",
+            options: [
+                    "George O'Malley",
+                    "Jo Wilson",
+                    "Teddy Altman",
+                    "Izzie stevens"
+        ]
+    },
+    {
+        numb: 11,
+            question: "Um paciente deu entrada no Seattle Grace Hospital com uma bomba alojada em sua cavidade torácica, logo, todos os cirurgiões receberam um bip em seu page dizendo:",
+            answer: "código Preto",
+            options: [
+                    "código Preto",
+                    "código bomba",
+                    "código Vermelho",
+                    "código Azul"
+        ]
+    },
+    {
+        numb: 12,
+            question: "O paciente Denny Duquette precisava de um coração novo, mas houve uma disputa entre dois cirurgiões para conseguir o coração. Quem era esses dois cirurgiões?",
+            answer: "Dr. Burker e Dra Hanh",
+            options: [
+                    "Dr. Burker e Dra. Hanh",
+                    "Dr. Shepherd e Dr. Burker",
+                    "Dra. Grey e Dra. kepner",
+                    "Dr. Hunt e Dr Avery"
+        ]
+    },
+    {
+        numb: 13,
+            question: "Todo mundo sabe quem é McDreamy, mas quem inventou o apelido?",
+            answer: "Cristina Yang",
+            options: [
+                    "Callie Torres",
+                    "Lexie Grey",
+                    "Cristina Yang",
+                    "Meredith Grey"
+        ]
+    },
+    {
+        numb: 14,
+            question: "Qual era o nome do paciente que a Teddy Altman se casou?",
+            answer: "Henry",
+            options: [
+                    "Diego",
+                    "Lukas",
+                    "Henry",
+                    "Diogo"
+        ]
+    },
+    {
+        numb: 15,
+            question: "Qual apelido Meredith recebeu dos internos?",
+            answer: "Medusa",
+            options: [
+                    "Nazista",
+                    "Mulher Maravilha",
+                    "Safadinha",
+                    "Medusa"
+        ]
+    },
+
+];
---
+++ script.js
@@ -0,0 +1,107 @@
+//Obtendo todos os elementos necessários
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+
+const option_list = document.querySelector(".option_list");
+
+// se o botão de iniciar o questionário foi clicado
+start_btn.onclick = ()=>{
+    info_box.classList.add("activeInfo"); //mostrar a caixa de informação
+}
+
+// se o botão de sair for clicado
+exit_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); //não mostrar a caixa de informação
+}
+
+// se o botão continue for clicado
+continue_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); //não mostrar a caixa de informação
+    quiz_box.classList.add("activeQuiz"); //mostrar a caixa de informação
+    showQuestions(0);
+    queCounter(1);
+}
+
+let que_count = 0;
+let que_numb = 1;
+let counter
+
+const next_btn = quiz_box.querySelector(".next_btn");
+const result_box = document.querySelector(".result_box");
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
+//Se o botão Next for clicado
+next_btn.onclick = ()=>{
+    if (que_count < questions.length - 1){
+        que_count++;
+        que_numb++;
+        showQuestions(que_count);
+        queCounter(que_numb);
+        next_btn.style.display = "none";
+    }else{
+        console.log("Questions completed");
+        showResultBox();
+    }
+}
+
+// recebendo perguntas e opções de array
+function showQuestions(index){
+    const que_text = document.querySelector(".que_text");
+    let que_tag = '<span>'+ questions[index].numb +"." + questions[index].question +'</span>';
+    let option_tag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
+                     + '<div class="option">'+ questions[index].options[1] +'<span></span></div>'
+                     + '<div class="option">'+ questions[index].options[2] +'<span></span></div>'
+                     + '<div class="option">'+ questions[index].options[3] +'<span></span></div>';
+    que_text.innerHTML = que_tag;
+    option_list.innerHTML = option_tag;
+
+    const option = option_list.querySelectorAll(".option");
+    for (let i = 0; i < option.length; i++) {
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
+
+function optionSelected(answer){
+    let userAns = answer.textContent;
+    let correctAns = questions[que_count].answer;
+    let allOptions = option_list.children.length;
+    if(userAns == correctAns){
+        answer.classList.add("correct");
+        console.log("Answer is correct");
+    }else{
+        answer.classList.add("incorrect");
+        console.log("Answer is wrong");
+    
+    //se a resposta estiver incorreta, selecione automaticamente a resposta correta
+        for (let i = 0; i < allOptions; i++) {  
+            if(option_list.children[i].textContent == correctAns){
+                option_list.children[i].setAttribute("class", "option correct");
+            }
+        }
+    }
+    
+    //uma vez que o usuário selecionou, desabilitou todas as opções
+    for (let i = 0; i < allOptions; i++) {
+        option_list.children[i].classList.add("disabled");
+    }
+    next_btn.style.display = "block";
+}
+function showResultBox(){
+        info_box.classList.remove("activeInfo");
+        quiz_box.classList.remove("activeQuiz");
+        result_box.classList.add("activeResult");
+}
+
+
+
+
+
+
+function queCounter(index){
+    const bottom_ques_counter = quiz_box.querySelector(".total_que");
+    let totalQues_countTag = '<span><p>' + index +'</p>of<p>' + questions.length + '</p>Questions</span>';
+    bottom_ques_counter.innerHTML = totalQues_countTag;
+}
